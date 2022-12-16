7a276cea656943323840ecf7404afdfcebe46bfb
USER <2020813704@student.uitm.edu.my>
2022-12-16 20:02:14 +0800

    zakatApps

--- app/src/main/java/net/akhma/zakatapps/About.java
+++ app/src/main/java/net/akhma/zakatapps/About.java
@@ -2,13 +2,31 @@ package net.akhma.zakatapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
+
+    TextView About_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        About_text = findViewById(R.id.About_text) ;
+
+        About_text.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                String urlGit = "https://github.com/AkhmaLail/zakatApps.git";
+                Intent in = new Intent(Intent.ACTION_VIEW);
+                in.setData(Uri.parse(urlGit));
+                startActivity(in);
+            }
+        });
     }
 }
